e2b126da948c6bb3efc9737fe19b933e254fb651
tytaka <tytakayanagi@nissho-ele.co.jp>
2022-10-20 16:35:40 +0700

    d

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World succeeded request');
+  res.send('Hello World succeeded request111');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
