2a0952608d31b4b542d48a60063515ef16aa257a
tytaka <tytakayanagi@nissho-ele.co.jp>
2022-10-20 16:44:35 +0700

    d

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 // Constants
 const PORT = process.env.PORT;
-const HOST = '0.0.0.0';
+const HOST = process.env.HOST;
 // App
 const app = express();
 app.get('/', (req, res) => {
